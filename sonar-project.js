75ba16eeb39bd5ec1da839e7a835050eaef5954e
kiranmai nayakanti <107303997+kiranmai2323@users.noreply.github.com>
2022-09-12 11:24:22 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'admin',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
